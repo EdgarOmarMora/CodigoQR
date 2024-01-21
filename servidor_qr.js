8f518c84dd8d33fad4cdb8847af1c99b59bc34ce
Edgar Mora <ecedgaromar@gmail.com>
2024-01-20 17:48:02 -0700

    QR String image

--- servidor_qr.js
+++ servidor_qr.js
@@ -36,6 +36,24 @@ app.get('/qrcode/:data',(req,res)=>{
     });
 });
 
+//Define QR generation route.
+app.get('/qrcode2/:data',(req,res)=>{
+    //Define the URL that we would like to convert into qr code.
+    const url='https://verificacfdi.facturaelectronica.sat.gob.mx/?'+req.params.data;
+
+    //Convert URL->dataURL (QR image representation).
+    QRCode.toDataURL(url,(err,qrCodeUrl)=>{
+        //Handle QR code generation errors.
+        //Conditional statement.
+        if(err){
+            //If there is an error we will an error response '500 Internal server error'.
+            res.status(500).send('Internal Server Error');
+        }else{
+            res.send(`${qrCodeUrl}`);
+        }
+    });
+});
+
 //Start the server and listen to request.
 app.listen(port, ()=>{
     console.log(`Server is runing on port http://localhost:${port}/qrcode`);
